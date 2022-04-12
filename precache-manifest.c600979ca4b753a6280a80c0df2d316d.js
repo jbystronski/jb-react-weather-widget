e36d3d564baad5d6d480ab6414f4fe84419a4e6c
Jakub Bystroński <jbstr85@gmail.com>
2022-04-12 03:20:52 +0200

    Updates

---
+++ precache-manifest.c600979ca4b753a6280a80c0df2d316d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd3b09236802c60d6e610238946daa6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39022fdf7a9f14b6ae38",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "b05d0e7f93f7f8392dec",
+    "url": "./static/js/2.5870e091.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.5870e091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39022fdf7a9f14b6ae38",
+    "url": "./static/js/main.f6511969.chunk.js"
+  },
+  {
+    "revision": "460fb11b29e16bac76f0",
+    "url": "./static/js/runtime-main.5824c806.js"
+  }
+]);
